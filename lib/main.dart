f920d14c0e33d4aae6f861c546b1209ecde38cb4
Dominik Wędzina <dominik.wedzina@onet.pl>
2025-03-25 17:41:10 +0100

    add icon to button, change styles

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,23 @@ import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 85, 29, 183),
+                Color.fromARGB(255, 134, 29, 183),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
